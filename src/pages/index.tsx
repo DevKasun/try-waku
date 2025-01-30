e126af6a2580a9e304c08de1cabf435e9842ba75
Kasun Lakshitha <kasun2014@outlook.com>
2025-01-30 23:58:21 +0530

    installed the dotenv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,15 @@ import { ProtectedRoute } from '../components/ProtectedRoute';
 
 export default async function HomePage() {
 	return (
-		<ProtectedRoute>
 			<div className="flex flex-col items-center justify-center space-y-4">
 				<h1 className="text-4xl font-bold">Welcome to Home Page</h1>
 				<p className="text-lg">This is a protected page. Only authenticated users can see this content.</p>
 			</div>
-		</ProtectedRoute>
 	);
 }
 
 export const getConfig = async () => {
 	return {
-		render: 'dynamic',
+		render: 'static',
 	} as const;
 };
